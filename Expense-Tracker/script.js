8e00a6393fee9ba7c5cd8b2593c26a2863c92e66
Sasa Nedic <svetasmak0@gmail.com>
2024-01-27 14:15:16 +0100

    Create class Budget with constructor

--- Expense-Tracker/script.js
+++ Expense-Tracker/script.js
@@ -1,6 +1,24 @@
 const budgetInput = document.querySelector(".budget-input");
 const budgetBtn = document.querySelector(".set-budget-btn");
 
+//globalna koji mi je balance i koji mi je array expensova
+//a da bi napravio taj expense moram da napravim ono new Expense
+//svaki put kad dodam pravim novi expense i gurnem to u array
+class Budget {
+  constructor() {
+    this.budget = 0;
+    this.expenses = 0;
+    this.balance = 0;
+    this.expenseArray = [];
+  }
+}
+// const UX = {
+//     displayBudget(){
+
+//     }
+// }
+const BUDGET = new Budget();
+
 budgetBtn.addEventListener("click", function () {
   console.log(budgetInput.value);
   const budgetValue = budgetInput.value;
