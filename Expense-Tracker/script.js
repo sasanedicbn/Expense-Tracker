48cddaf75e7cea3e231381090b159c47bbcfdfca
Sasa Nedic <svetasmak0@gmail.com>
2024-01-27 14:07:36 +0100

    Select variables and add event listener

--- Expense-Tracker/script.js
+++ Expense-Tracker/script.js
@@ -1,5 +1,7 @@
 const budgetInput = document.querySelector(".budget-input");
 const budgetBtn = document.querySelector(".set-budget-btn");
+
 budgetBtn.addEventListener("click", function () {
   console.log(budgetInput.value);
+  const budgetValue = budgetInput.value;
 });
